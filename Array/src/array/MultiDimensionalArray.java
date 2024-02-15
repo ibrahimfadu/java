b016c0d21405f402224b8619263dd366bcc418db
ibrahimfadu <152989432+ibrahimfadu@users.noreply.github.com>
2024-02-15 08:02:33 +0530

    Update MultiDimensionalArray.java

--- Array/src/array/MultiDimensionalArray.java
+++ Array/src/array/MultiDimensionalArray.java
@@ -3,7 +3,7 @@ package array;
 public class MultiDimensionalArray {
 
 	public static void main(String[] args) {
-		int arr[][]= {{1,2,3},{2,4,5},{4,4,5}};
+		int arr[][]= {{1,2,3,7},{2,4,5},{4,4,5}};
 		for (int i=0;i<3;i++)
 		{
 			for (int j=0;j<3;j++) {
